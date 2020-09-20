f902a15f29fb2b156fc3609f33939b10b77346ef
lordkettune <lefox2@gmail.com>
2020-09-20 16:35:31 -0700

    Pattern object

--- src/kdanmaku.cpp
+++ src/kdanmaku.cpp
@@ -1,5 +1,7 @@
 #include <Godot.hpp>
 
+#include "pattern.hpp"
+
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options* o)
 {
     godot::Godot::gdnative_init(o);
@@ -13,4 +15,6 @@ extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_opt
 extern "C" void GDN_EXPORT godot_nativescript_init(void* handle)
 {
     godot::Godot::nativescript_init(handle);
+
+    godot::register_class<godot::Pattern>();
 }
